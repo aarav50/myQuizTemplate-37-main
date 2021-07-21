58749dd32c956064593d04924f0019c3c3431dca
aarav50 <aarav.sai20@gmail.com>
2021-07-20 20:43:19 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canva;
+var bbb
+var database
+function setup(){
+  database=firebase.database()
+  bbb=new bb()
+  canvas = createCanvas(850,400);
+  
+}
+
+
+function draw(){
+  background("pink");
+bbb.display()
+
+  
+}
